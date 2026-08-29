7448a6886931b4f9b8784421878e1c7a2de21dcb
Yazdan Sadri <yazdansdr@gmail.com>
2026-08-29 15:02:22 +0330

    react icons uninstallation

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,15 +1,5 @@
-import { SiFigma, SiSketch, SiBlender, SiCoreldraw } from "react-icons/si";
-import type { IconType } from "react-icons";
+const skills = () => {
+    return <div>this is skills data component</div>;
+};
 
-export interface Skill {
-    name: string;
-    icon: IconType;
-    color: string;
-}
-
-export const skills: Skill[] = [
-    { name: "Figma", icon: SiFigma, color: "#F24E1E" },
-    { name: "Sketch", icon: SiSketch, color: "#F7B500" },
-    { name: "Blender", icon: SiBlender, color: "#E87D0D" },
-    { name: "CorelDRAW", icon: SiCoreldraw, color: "#7DAB43" },
-];
+export default skills;
