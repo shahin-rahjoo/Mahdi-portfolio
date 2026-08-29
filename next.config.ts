7a0f8037b6ff4c06f40f311252d40a5b418572e7
Yazdan Sadri <yazdansdr@gmail.com>
2026-08-29 22:47:18 +0330

    bilingual feature added(in progress)

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
-import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {
-    /* config options here */
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "cdn.simpleicons.org",
-            },
-        ],
-    },
-};
+const withNextIntl = createNextIntlPlugin();
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default withNextIntl(nextConfig);
